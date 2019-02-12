4fdab1a6243640130c82b9d5f89796ac331c17a4
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-02-12 23:30:39 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -39,7 +39,7 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-    for message in 1..number_of_times
+    for message in 0..number_of_times
     puts phrase
   end
 end
